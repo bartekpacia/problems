d07754dbe402a37bcbc1cc6f3fb81c100b6e9cbd
Bartek Pacia <barpac02@gmail.com>
2020-12-02 01:44:57 +0100

    bus error, let's call it a day

--- polsl/main.c
+++ polsl/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int numberOfDigits(int num) {
+  int digits = 0;
+  while (num != 0) {
+    num = num / 10;
+    digits++;
+  }
+
+  return digits;
+}
+
+int solve(int countOfPlates) {
+  int numberOfBuildings = 1;
+  while (countOfPlates > 0) {
+    int numOfDigits = numberOfDigits(numberOfBuildings);
+    countOfPlates -= numOfDigits;
+
+    numberOfBuildings++;
+  }
+
+  if (countOfPlates != 0) {
+    return -1;  // let's signal that it's invalid
+  }
+
+  int countOfBuildings = numberOfBuildings - 1;
+  return countOfBuildings;
+}
+
 int main() {
   int plateNumbers[10] = {0};
 
@@ -17,11 +44,22 @@ int main() {
 
     i++;
   }
+  fclose(inputFile);
 
   FILE* outputFile = fopen("num-01.out", "w");
   for (int i = 0; i < 10; i++) {
-    printf("%i: %i\n", i, plateNumbers[i]);
+    if (plateNumbers[i] == 0) {
+      break;
+    }
+
+    int buildingCount = solve(plateNumbers[i]);
+    // int size = numberOfDigits(buildingCount);
+    // char str[size];
+    // sprintf(str, "%d", buildingCount);
+
+    fprintf(outputFile, "%i", buildingCount);
   }
+  fclose(outputFile);
 
   return 0;
 }
