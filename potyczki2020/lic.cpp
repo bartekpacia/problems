664713a14420e81cc4d00afb6ad98569557db288
Bartek Pacia <barpac02@gmail.com>
2020-12-04 21:04:40 +0100

    add some scaffold

--- potyczki2020/lic.cpp
+++ potyczki2020/lic.cpp
@@ -1,23 +1,41 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
+vector<int> digitsOf(int n) {
+  vector<int> digits;
+
+  while (n != 0) {
+    int digit = n % 10;
+    digits.push_back(digit);
+    n = n / 10;
+  }
+
+  reverse(digits.begin(), digits.end());
+  return digits;
+}
+
+bool isPotychnikovNumber(int num) { return true; }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  vector<int> input;
+  int l, r;
+  cin >> l >> r;
 
-  while (!cin.eof()) {
-    int val;
-    cin >> val;
-    input.push_back(val);
+  for (int num = l; num <= r; num++) {
+    if (isPotychnikovNumber(num)) {
+      cout << num << endl;
+    }
   }
 
-  cout << endl;
+  auto digits = digitsOf(1234);
 
-  for (int x : input) {
-    cout << x << endl;
+  for (int digit : digits) {
+    cout << digit << endl;
   }
 
   return 0;
