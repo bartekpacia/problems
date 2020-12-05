1e0b535bb65c283af9903bc036c4cbf1490f6909
Bartek Pacia <barpac02@gmail.com>
2020-12-05 17:14:53 +0100

    it works for not-gigantic l and r

--- potyczki2020/lic.cpp
+++ potyczki2020/lic.cpp
@@ -1,14 +1,26 @@
+#include <math.h>
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+/**
+ * Custom method for this task.
+ * Returns a vector of digits of n, from left to right.
+ * If n has any 0, returns an empty vector.
+ */
 vector<int> digitsOf(int n) {
   vector<int> digits;
 
   while (n != 0) {
     int digit = n % 10;
+
+    if (digit == 0) {
+      return vector<int>{};
+    }
+
     digits.push_back(digit);
     n = n / 10;
   }
@@ -17,26 +29,50 @@ vector<int> digitsOf(int n) {
   return digits;
 }
 
-bool isPotychnikovNumber(int num) { return true; }
+int sumOfVector(vector<int> nums) {
+  int sum = 0;
+  for (int num : nums) {
+    sum += num;
+  }
+
+  return sum;
+}
+
+bool isPotychnikovNumber(int num) {
+  // Fail early.
+  if (num % 10 == 0) {
+    return false;
+  }
+
+  vector<int> digits = digitsOf(num);
+
+  if (digits.empty()) {
+    return false;
+  }
+
+  for (int digit : digits) {
+    if (num % digit != 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
+  int sum = 0;
   int l, r;
   cin >> l >> r;
-
   for (int num = l; num <= r; num++) {
     if (isPotychnikovNumber(num)) {
-      cout << num << endl;
+      sum++;
     }
   }
 
-  auto digits = digitsOf(1234);
-
-  for (int digit : digits) {
-    cout << digit << endl;
-  }
+  cout << sum << endl;
 
   return 0;
 }
