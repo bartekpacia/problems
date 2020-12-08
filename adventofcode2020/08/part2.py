a22e18d2098670dac1acf263405c25a5b3ddab40
Bartek Pacia <barpac02@gmail.com>
2020-12-08 19:14:46 +0100

    getting rid of global state

--- adventofcode2020/08/part2.py
+++ adventofcode2020/08/part2.py
@@ -1,7 +1,4 @@
-i = 1
-acc = 0
 lines: list[str] = []
-executed = set()
 
 def parse_instruction(line: str) -> (str, int):
   line = line.split()
@@ -10,46 +7,30 @@ def parse_instruction(line: str) -> (str, int):
 
   return cmd, num
 
-def parse(prev, current):
-  global acc, i
+def parse(acc: int, executed: set[int], prev: int, current: int) -> int | None:
+  # if not executed:
+  #  print("empty")
+  #else:
+  #  print(f"size of executed is {len(executed)}")
 
   if current == len(lines):
-    print(f"reached last line! {acc=}, execution suspended")
-    return
-    
+    return acc
+
   if current in executed:
-    print(f"infinite loop detected! line {current}, {acc=}, execution suspended")
+    # print(f"abort")
     return
-    # print(f"infinite loop detected! line {current}, {acc=}, trying to fix...")
-    # prev_cmd, prev_num = parse_instruction(lines[prev])
-    # print(f"prev_num: {prev_num:+}")
-    # if prev_cmd == "nop":
-    #   lines[prev] = f"jmp {prev_num:+}"
-    #   parse(current, prev)
-
-    # elif prev_cmd == "jmp":
-    #   lines[prev] = f"nop {prev_num:+}"
-    #   parse(current, prev)
-
-    # else:
-    #   print(f"wtf... {prev_cmd=}")
-    #   return
-    
-    # return
 
   executed.add(current)
   cmd, num = parse_instruction(lines[current])
-
-  print(f"{i} parse({current}), {cmd} {num:+} {acc=}")
   
-  i += 1
   if cmd == "nop":
-    parse(current, current + 1)
+    res = parse(acc, executed, current, current + 1)
   elif cmd == "acc":
-    acc += num
-    parse(current, current + 1)
+    res = parse(acc + num, executed, current, current + 1)
   elif cmd == "jmp":
-    parse(current, current + num)
+    res = parse(acc, executed, current, current + num)
+
+  return res
 
 def generate_all_possible_sets():
   test_lists: list[list[str]] = []
@@ -71,21 +52,17 @@ def generate_all_possible_sets():
   return test_lists
 
 def main():
-  global lines, executed, acc
+  global lines
   with open("input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
-
   
   test_lists = generate_all_possible_sets()
-
   for l in test_lists:
-    acc = 0
-    executed = set()
-    for i, elem in enumerate(l):
-      if "acc" not in elem:
-        print(i, elem)
+    # reset global state
     lines = l
-    parse(0, 0)
 
+    acc = parse(0, set(), 0, 0)
+    if acc:
+      print(f"{acc=}")
 
 main()
