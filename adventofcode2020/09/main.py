7b3568a5b1d90612a59ad9a591cc6f431688136d
Bartek Pacia <barpac02@gmail.com>
2020-12-09 09:57:37 +0100

    kinda works

--- adventofcode2020/09/main.py
+++ adventofcode2020/09/main.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-PREAMBLE_LENGTH = 25
+PREAMBLE_LENGTH = 5
 
 def solve_part_1(lines: list[str]) -> int:
   invalid = None
@@ -10,7 +10,6 @@ def solve_part_1(lines: list[str]) -> int:
     last_segment.append(lines[i])
 
   for i in range(PREAMBLE_LENGTH, len(lines)):
-    print(f"iterating at {i}")
     num = lines[i]
 
     found_match = False
@@ -32,12 +31,31 @@ def solve_part_1(lines: list[str]) -> int:
 
   return invalid
 
+def solve_part_2(lines: list[str], target_num: int) -> int:
+  begin = 0
+  end = 1
+
+  while begin < len(lines):
+    s = 0
+    for i in range(begin, end):
+      s += lines[i]
+
+    if s < target_num:
+      end += 1
+    elif s > target_num:
+      begin += 1
+    else:
+      print(f"found range: {lines[begin:end]}")
+      return lines[begin] + lines[end]
+  
 
 def main():
-  with open("input.txt") as f:
+  with open("test.txt") as f:
     lines = [int(l.strip()) for l in f.readlines()]
 
   invalid_num = solve_part_1(lines)
-  print(len(lines))
+  print(f"INVALID: {invalid_num}")
+  weakness = solve_part_2(lines, invalid_num)
+  print(f"weakness (part 2): {weakness}")
 
 main()
