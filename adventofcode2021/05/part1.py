cb4d5558c85f1fc5f02d96f7f407590c6b49b80d
Bartek Pacia <barpac02@gmail.com>
2021-12-05 13:55:40 +0100

    adventofcode2021: task 5 part 1: logic

--- adventofcode2021/05/part1.py
+++ adventofcode2021/05/part1.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 # list of vectors
 vectors: list[tuple[tuple[int, int], tuple[int, int]]] = []
 
@@ -7,8 +9,58 @@ with open("sample.txt") as file:
         x1, y1 = tuple(map(lambda x: int(x), seg[0].strip().split(",")))
         x2, y2 = tuple(map(lambda x: int(x), seg[1].strip().split(",")))
 
+        # for now, only consider horizontal and vertical lines
+        if x1 != x2 and y1 != y2:
+            continue
+
         v = ((x1, y1), (x2, y2))
         vectors.append(v)
 
-for v in vectors:
-    print(v)
+
+def dist(point1: tuple[int, int], point2: tuple[int, int]) -> int:
+    # sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2
+    val = (point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2
+    return int(sqrt(val))  # will always be int in part 1
+
+
+def find_line_points(
+    point1: tuple[int, int],
+    point2: tuple[int, int],
+) -> list[tuple[int, int]]:
+    points: list[tuple[int, int]] = []
+    x1, y1 = point1
+    x2, y2 = point2
+    if x1 == x2:
+        # vertical line
+        print(f"vertical dist: {d}")
+        if y1 < y2:
+            while y1 <= y2:
+                points.append((x1, y1))
+                y1 += 1
+        elif y1 > y2:
+            while y2 <= y1:
+                points.append((x1, y1))
+                y1 -= 1
+    elif y1 == y2:
+        if x1 < x2:
+            while x1 <= x2:
+                points.append((x1, y1))
+                x1 += 1
+        elif x1 > x2:
+            while x2 <= x1:
+                points.append((x1, y1))
+                x1 -= 1
+
+        # horizontal line
+        print(f"horizontal dist: {d}")
+
+    return points
+
+
+for point1, point2 in vectors:
+    d = dist(point1, point2)
+    points = find_line_points(point1, point2)
+
+    if points:
+        for point in points:
+            print(point)
