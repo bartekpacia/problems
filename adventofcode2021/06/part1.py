2828fc26ddec62537a3d3671124d13ee9582f4a3
Bartek Pacia <barpac02@gmail.com>
2021-12-06 17:48:24 +0100

    clean up

--- adventofcode2021/06/part1.py
+++ adventofcode2021/06/part1.py
@@ -3,7 +3,6 @@ with open("input.txt") as file:
     fish = list(map(lambda x: int(x), file.readline().split(",")))
 
 days = 80
-print(f"Initial state: {fish}")
 for d in range(days):
     for i in range(len(fish)):
         if fish[i] == 0:
@@ -13,6 +12,4 @@ for d in range(days):
         else:
             fish[i] -= 1
 
-    print(f"After {d+1:<3} day: {fish}")
-
-print(f"after {days} days, there will be {len(fish)} fish")
+print(len(fish))
