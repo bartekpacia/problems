cb64542277e644c623ccfb8fc012ad81bc731e00
Bartek Pacia <barpac02@gmail.com>
2021-12-07 14:58:45 +0100

    clean up

--- adventofcode2021/07/part1.py
+++ adventofcode2021/07/part1.py
@@ -8,26 +8,13 @@ for ref_pos in crab_positions:
     ref_fuel = 0
     for pos in crab_positions:
         fuel = abs(ref_pos - pos)
-        if ref_pos == 2:
-            print(f"Move from {pos} to {ref_pos}: {fuel} fuel")
         ref_fuel += fuel
 
-    print(f"{ref_fuel=}")
     if min_fuel is None:
         min_pos = ref_pos
         min_fuel = ref_fuel
     elif ref_fuel < min_fuel:
-        print("ref_fuel < min_fuel")
         min_pos = ref_pos
         min_fuel = ref_fuel
 
-
-# mapping of position to crab count in that position
-# crabs: dict[int, int] = {}
-# for ref_pos in crab_positions:
-#     if ref_pos not in crabs:
-#         crabs[ref_pos] = 1
-#     else:
-#         crabs[ref_pos] += 1
-
-print(min_pos, min_fuel)
+print(min_fuel)
