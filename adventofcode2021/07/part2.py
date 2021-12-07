31fb3707bf5fc51e9b72f7865d7a0e4f8cd1dfa0
Bartek Pacia <barpac02@gmail.com>
2021-12-07 15:11:21 +0100

    adventofcode2021: do task 7 part 2

---
+++ adventofcode2021/07/part2.py
@@ -0,0 +1,23 @@
+crab_positions: list[int] = []
+with open("input.txt") as file:
+    crab_positions = list(map(lambda x: int(x), file.readline().split(",")))
+
+min_pos: int | None = None
+min_fuel: int | None = None
+for ref_pos in range(len(crab_positions)):
+    ref_fuel = 0
+    for pos in crab_positions:
+        dist = abs(ref_pos - pos)
+        fuel = sum(range(dist + 1))
+        if ref_pos == 5:
+            print(f"Move from {pos} to {ref_pos}: {fuel} fuel")
+        ref_fuel += fuel
+
+    if min_fuel is None:
+        min_pos = ref_pos
+        min_fuel = ref_fuel
+    elif ref_fuel < min_fuel:
+        min_pos = ref_pos
+        min_fuel = ref_fuel
+
+print(min_fuel)
