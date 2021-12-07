8775a8c1d8696413a38365d6e62185a6909c73f3
Bartek Pacia <barpac02@gmail.com>
2021-12-07 15:12:08 +0100

    clean up

--- adventofcode2021/07/part1.py
+++ adventofcode2021/07/part1.py
@@ -2,7 +2,6 @@ crab_positions: list[int] = []
 with open("input.txt") as file:
     crab_positions = list(map(lambda x: int(x), file.readline().split(",")))
 
-min_pos: int | None = None
 min_fuel: int | None = None
 for ref_pos in crab_positions:
     ref_fuel = 0
@@ -11,10 +10,8 @@ for ref_pos in crab_positions:
         ref_fuel += fuel
 
     if min_fuel is None:
-        min_pos = ref_pos
         min_fuel = ref_fuel
     elif ref_fuel < min_fuel:
-        min_pos = ref_pos
         min_fuel = ref_fuel
 
 print(min_fuel)
