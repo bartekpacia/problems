4bedd95b495f6cf34e9a1711eabc9f0c2956da55
Bartek Pacia <barpac02@gmail.com>
2021-12-07 14:58:13 +0100

    adventofcode2021: do task 7 part 1

--- adventofcode2021/07/part1.py
+++ adventofcode2021/07/part1.py
@@ -1,13 +1,33 @@
 crab_positions: list[int] = []
-with open("sample.txt") as file:
+with open("input.txt") as file:
     crab_positions = list(map(lambda x: int(x), file.readline().split(",")))
 
-# mapping of position to crab count in that position
-crabs: dict[int, int] = {}
-for pos in crab_positions:
-    if pos not in crabs:
-        crabs[pos] = 1
-    else:
-        crabs[pos] += 1
+min_pos: int | None = None
+min_fuel: int | None = None
+for ref_pos in crab_positions:
+    ref_fuel = 0
+    for pos in crab_positions:
+        fuel = abs(ref_pos - pos)
+        if ref_pos == 2:
+            print(f"Move from {pos} to {ref_pos}: {fuel} fuel")
+        ref_fuel += fuel
 
-print(crabs)
+    print(f"{ref_fuel=}")
+    if min_fuel is None:
+        min_pos = ref_pos
+        min_fuel = ref_fuel
+    elif ref_fuel < min_fuel:
+        print("ref_fuel < min_fuel")
+        min_pos = ref_pos
+        min_fuel = ref_fuel
+
+
+# mapping of position to crab count in that position
+# crabs: dict[int, int] = {}
+# for ref_pos in crab_positions:
+#     if ref_pos not in crabs:
+#         crabs[ref_pos] = 1
+#     else:
+#         crabs[ref_pos] += 1
+
+print(min_pos, min_fuel)
