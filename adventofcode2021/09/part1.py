e14df38cf12dc084899a46bd72d2079195e465f9
Bartek Pacia <barpac02@gmail.com>
2021-12-09 21:26:22 +0100

    adventofcode2021: do task 9 part 1

--- adventofcode2021/09/part1.py
+++ adventofcode2021/09/part1.py
@@ -1,8 +1,38 @@
 with open("input.txt") as file:
     heightmap = [[int(num) for num in row.strip()] for row in file.readlines()]
 
+
+def is_low_point(i: int, j: int) -> bool:
+    point = heightmap[i][j]
+    adjacent_points: list[int] = []
+    if j - 1 >= 0:
+        left = heightmap[i][j - 1]
+        adjacent_points.append(left)
+
+    if i - 1 >= 0:
+        top = heightmap[i - 1][j]
+        adjacent_points.append(top)
+
+    if j + 1 < len(heightmap[i]):
+        right = heightmap[i][j + 1]
+        adjacent_points.append(right)
+
+    if i + 1 < len(heightmap):
+        bottom = heightmap[i + 1][j]
+        adjacent_points.append(bottom)
+
+    for adj_point in adjacent_points:
+        if not point < adj_point:
+            return False
+
+    return True
+
+
+risk = 0
 for i in range(len(heightmap)):
     for j in range(len(heightmap[i])):
         point = heightmap[i][j]
-        print(point, end="")
-    print()
+        if is_low_point(i, j):
+            risk += 1 + point
+
+print(risk)
