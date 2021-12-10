b9e6458b95bf288283139a1f12f1c81e1eddb10c
Bartek Pacia <barpac02@gmail.com>
2021-12-10 17:57:41 +0100

    adventofcode2021: task 10 part 1: sample almost works

--- adventofcode2021/10/part1.py
+++ adventofcode2021/10/part1.py
@@ -1,37 +1,31 @@
 with open("sample.txt") as f:
     lines = [line.strip() for line in f]
 
+open_matches: dict[str, str] = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
+}
+
+close_matches = {v: k for k, v in open_matches.items()}
+
+
 for line in lines:
     # ignore incomplete lines
     if len(line) % 2 != 0:
         continue
 
-    stack_1: list[str] = [] # (
-    stack_2: list[str] = [] # [
-    stack_3: list[str] = [] # {
-    stack_4: list[str] = [] # <
-    
+    stack: list[str] = []
     for char in line:
-        # (
-        if char == "(":
-            stack_1.append(char)
-        elif char == ")":
-            stack_1.pop()
-
-        # [
-        if char == "[":
-            stack_2.append(char)
-        elif char == "]":
-            stack_2.pop()
-
-        # {
-        if char == "{":
-            stack_2.append(char)
-        elif char == "}":
-            stack_2.pop()
-        
-        # <
-        if char == "<":
-            stack_2.append(char)
-        elif char == ">":
-            stack_2.pop()
+        if char in open_matches.keys():
+            # opening brace
+            stack.append(char)
+        else:
+            # closing brace
+            if char == open_matches[stack[-1]]:
+                print(f"closing brace: {char=}")
+                stack.pop()
+            else:
+                print(f"Expected {open_matches[stack[-1]]}, but found {char} instead")
+                break
