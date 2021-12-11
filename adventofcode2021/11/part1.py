412104960e9850cc252461fafd1018f5ddee8f10
Bartek Pacia <barpac02@gmail.com>
2021-12-11 17:44:37 +0100

    adventofcode2021: progress on task 11 part 1: we're home haha!
    it was a stupid, off-by-one comparison
    heh

--- adventofcode2021/11/part1.py
+++ adventofcode2021/11/part1.py
@@ -5,12 +5,6 @@ with open("sample.txt") as f:
         matrix.append(row)
 
 
-def update_all():
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix[i][j] += 1
-
-
 def print_matrix():
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
@@ -19,31 +13,89 @@ def print_matrix():
         print()
 
 
-def flash():
+def do_step():
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if j - 1 >= 0:
-                # left
-                matrix[i][j - 1] += 1
+            matrix[i][j] += 1
 
-            if j - 1 >= 0 and i - 1 >= 0:
-                # left-top
-                matrix[j - 1][i - 1] += 1
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] > 9 and (i, j) not in already_flashed:
+                flash(i, j)
 
-            if i - 1 >= 0:
-                # top
-                matrix[i - 1][j] += 1
-
-            if j + 1 < len(matrix[i]):
-                # right
-                matrix[i][j + 1] += 1
-            if i - 1 >= 0 and j + 1 < len(matrix[i]):
-                # right-top
-                matrix[i - 1][j + 1] += 1
-
-            if i + 1 < len(matrix):
-                # bottom
-                matrix[i + 1][j] += 1
+    for i, j in already_flashed:
+        matrix[i][j] = 0
 
 
-step = 0
+flash_count = 0
+already_flashed: set[tuple[int, int]] = set()
+
+
+def flash(i: int, j: int):
+    """
+    Increases the energy of all elements adjacent to [i][j] (there are max 8 of
+    them) by 1.
+    """
+    print(f"flashing ({i},{j})")
+    global flash_count
+    flash_count += 1
+    already_flashed.add((i, j))
+    if j - 1 >= 0:
+        # left
+        matrix[i][j - 1] += 1
+        if matrix[i][j - 1] > 9 and (i, j - 1) not in already_flashed:
+            flash(i, j - 1)
+
+    if i - 1 >= 0 and j - 1 >= 0:
+        # left-top
+        matrix[i - 1][j - 1] += 1
+        if matrix[i - 1][j - 1] > 9 and (i - 1, j - 1) not in already_flashed:
+            flash(i - 1, j - 1)
+
+    if i - 1 >= 0:
+        # top
+        matrix[i - 1][j] += 1
+        if matrix[i - 1][j] > 9 and (i - 1, j) not in already_flashed:
+            flash(i - 1, j)
+
+    if i - 1 >= 0 and j + 1 < len(matrix[i]):
+        # right-top
+        matrix[i - 1][j + 1] += 1
+        if matrix[i - 1][j + 1] > 9 and (i - 1, j + 1) not in already_flashed:
+            flash(i - 1, j + 1)
+
+    if j + 1 < len(matrix[i]):
+        # right
+        matrix[i][j + 1] += 1
+        if matrix[i][j + 1] > 9 and (i, j + 1) not in already_flashed:
+            flash(i, j + 1)
+
+    if i + 1 < len(matrix) and j + 1 < len(matrix[i]):
+        # right-bottom
+        matrix[i + 1][j + 1] += 1
+        if matrix[i + 1][j + 1] > 9 and (i + 1, j + 1) not in already_flashed:
+            flash(i + 1, j + 1)
+
+    if i + 1 < len(matrix):
+        # bottom
+        matrix[i + 1][j] += 1
+        if matrix[i + 1][j] > 9 and (i + 1, j) not in already_flashed:
+            flash(i + 1, j)
+
+    if i + 1 < len(matrix) and j - 1 >= 0:
+        # left-bottom
+        matrix[i + 1][j - 1] += 1
+        if matrix[i + 1][j - 1] > 9 and (i + 1, j - 1) not in already_flashed:
+            flash(i + 1, j - 1)
+
+
+print("--- before any steps ---")
+print_matrix()
+
+for i in range(10):
+    already_flashed.clear()
+    do_step()
+    print(f"--- after step {i + 1} ---")
+    print_matrix()
+
+print(flash_count)
