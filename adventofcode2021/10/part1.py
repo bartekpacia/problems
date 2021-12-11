f20f9ecf49a7e30fb0b44687c8ad85182d0f53ca
Bartek Pacia <barpac02@gmail.com>
2021-12-11 11:12:16 +0100

    adventofcode2021: do 10 part 1

--- adventofcode2021/10/part1.py
+++ adventofcode2021/10/part1.py
@@ -1,4 +1,4 @@
-with open("sample.txt") as f:
+with open("input.txt") as f:
     lines = [line.strip() for line in f]
 
 open_matches: dict[str, str] = {
@@ -8,13 +8,20 @@ open_matches: dict[str, str] = {
     "<": ">",
 }
 
+scores: dict[str, int] = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+
 close_matches = {v: k for k, v in open_matches.items()}
+
+illegals = {k: 0 for k, _ in close_matches.items()}
 
 
 for i, line in enumerate(lines):
-    # ignore incomplete lines
-    if len(line) % 2 != 0:
-        continue
+    print(f"---------line {i+1}---------")
 
     stack: list[str] = []
     for char in line:
@@ -24,8 +31,14 @@ for i, line in enumerate(lines):
         else:
             # closing brace
             if char == open_matches[stack[-1]]:
-                print(f"closing brace: {char=}")
+                print(f"opened brace {stack[-1]} closed with {char}")
                 stack.pop()
             else:
                 print(f"Expected {open_matches[stack[-1]]}, but found {char} instead")
+                illegals[char] += 1
                 break
+score = 0
+for k, v in illegals.items():
+    score += scores[k] * v
+
+print(score)
