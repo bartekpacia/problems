1d2a2d864f22cd785524060f4371087024faee3c
Bartek Pacia <barpac02@gmail.com>
2022-12-01 18:52:02 +0100

    adventofcode2022: do day 1

---
+++ adventofcode2022/01/part1.py
@@ -0,0 +1,16 @@
+with open("input.txt") as file:
+    max_calories = int(file.readline().strip())
+    current_calories = 0
+    for l in file:
+        line = l.strip()
+        if not line:
+            if current_calories > max_calories:
+                max_calories = current_calories
+            current_calories = 0
+            continue
+            
+        calories = int(line)
+        current_calories += calories
+
+print(max_calories)
+
