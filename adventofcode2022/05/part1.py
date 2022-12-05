890268a936f199d231075ac4d3be226621efa12a
Bartek Pacia <barpac02@gmail.com>
2022-12-06 00:30:16 +0100

    adventofcode2022: day 5 part 1 works for real input!

--- adventofcode2022/05/part1.py
+++ adventofcode2022/05/part1.py
@@ -1,7 +1,7 @@
 stacks: list[list[str]] = []
 instructions: list[tuple[int, int, int]] = []
 
-with open("sample.txt") as file:    
+with open("input.txt") as file:
     matrix: list[list[str]] = []
 
     reading_instr = False
@@ -10,25 +10,25 @@ with open("sample.txt") as file:
         if not line.strip():
             reading_instr = True
             continue
-        
+
         if reading_instr:
             line = line.strip()
-            line = line.replace('move ', '')
-            line = line.replace('from ', '')
-            line = line.replace(' to', '')
+            line = line.replace("move ", "")
+            line = line.replace("from ", "")
+            line = line.replace(" to", "")
             numbers: list[str] = []
-            current_number = ''
+            current_number = ""
             for char in line:
                 if char.isnumeric():
                     current_number += char
                 else:
                     numbers.append(current_number)
-                    current_number = ''
-            
+                    current_number = ""
+
             numbers.append(current_number)
 
             numbers_list = [int(n) for n in numbers]
-            instr = (numbers_list[0], numbers_list[1]-1, numbers_list[2]-1)
+            instr = (numbers_list[0], numbers_list[1] - 1, numbers_list[2] - 1)
             instructions.append(instr)
         else:
             matrix.append([])
@@ -38,26 +38,20 @@ with open("sample.txt") as file:
     for i, char in enumerate(matrix[-1]):
         if char.isnumeric():
             stacks.append([])
-            for j in range(len(matrix)-2, -1, -1):
+            for j in range(len(matrix) - 2, -1, -1):
                 if matrix[j][i].isalpha():
                     stacks[-1].append(matrix[j][i])
-    
 
-for stack in stacks:
-    print(stack)
-
-# rearrange stack using on instructions
 
 def move(src: int, dst: int):
     stacks[dst].append(stacks[src].pop())
 
+
 for instr in instructions:
-    print(f'executing instruction {instr}')
     count = instr[0]
     src = instr[1]
     dst = instr[2]
     for i in range(count):
-        print(f'moving from {src} to {dst}')
         move(src, dst)
 
 
@@ -70,21 +64,7 @@ def rearrange(stacks: list[list[str]], instructions: list[tuple[int, int, int]])
                 stacks[int(char)][0] = char
                 break
 
-for stack in stacks:
-    print(stack)
-
-result=''
+result = ""
 for stack in stacks:
     result += stack[-1]
 print(result)
-
-# for i, _ in enumerate(matrix):
-#     for j, _ in enumerate(matrix[i]):
-#         print(matrix[i][j], end='')
-#     print('')
-
-#for row in containers_layout:
-#    print(row)
-
-# converts [D]
-#def read_char()
